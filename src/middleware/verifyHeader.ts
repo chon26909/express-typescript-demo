c543ff2c0cf0c2dc1f9a8df083f1284258c52c80
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-01-26 23:15:31 +0700

    update verify header

--- src/middleware/verifyHeader.ts
+++ src/middleware/verifyHeader.ts
@@ -8,12 +8,19 @@ interface DecodedToken {
 export const authentication = (req: Request, res: Response, next: NextFunction) => {
     const authorization = req.headers.authorization;
 
+    console.log('req', req.headers);
+
     const token = String(authorization).split('Bearer ')[1];
 
     try {
         const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
-        req.session.uid = decoded.uid;
-        next();
+        if (decoded.uid) {
+            console.log('decoded', decoded);
+            req.session.uid = decoded.uid;
+            next();
+        } else {
+            res.status(401).json({ message: 'Unauthorized' });
+        }
     } catch (error) {
         console.log('error', error);
         res.status(401).json({ message: 'Unauthorized' });
